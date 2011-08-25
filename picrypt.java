3c6322529948478ad7be67ef2798a503c17d25f2
jimmy hendrix <jimmythedestroyer@gmail.com>
2011-08-24 21:56:46 -0700

    test embedding/extracting and outputing a png

--- picrypt.java
+++ picrypt.java
@@ -12,31 +12,38 @@ class Picrypt extends JFrame
         img = ImageIO.read(new File("sample.jpg"));
     } catch (IOException e) { System.out.println(e); }
     
-    getPixel(550, 550);
-    
-    int pixel = 0;
-    int[] color = {0, 0, 0};
     for (int x=0; x < img.getWidth(); x++) {
       for (int y=0; y < img.getHeight(); y++) {
-        pixel = getRgbAsInt(color);
+        int[] pixel = getPixel(x, y);
+        
+        pixel[0] |= 0x00000003;
+        pixel[1] |= 0x00000003;
+        pixel[2] |= 0x00000003;
+        
         setPixel(x, y, pixel);
       }
     }
     
     try {
-      File outputfile = new File("output.jpg");
-      ImageIO.write(img, "jpg", outputfile);
+      File outputfile = new File("output.png");
+      ImageIO.write(img, "png", outputfile);
     } catch (IOException e) { System.out.println(e); }
-  }
-  
-  public int getRgbAsInt(int[] rgb) {
-    int pixel = 0xff000000;
     
-    pixel |= (rgb[0] << 16);
-    pixel |= (rgb[1] << 8);
-    pixel |= rgb[2];
+    try {
+        img = ImageIO.read(new File("output.png"));
+    } catch (IOException e) { System.out.println(e); }
     
-    return pixel;
+    for (int x=0; x < img.getWidth(); x++) {
+      for (int y=0; y < img.getHeight(); y++) {
+        int[] pixel = getPixel(x, y);
+        
+        pixel[0] &= 0x00000003;
+        pixel[1] &= 0x00000003;
+        pixel[2] &= 0x00000003;
+        
+        System.out.println("Red: " + pixel[0] + ", Green: " + pixel[1] + ", Blue: " + pixel[2]);
+      }
+    }
   }
   
   public int[] getPixel(int x, int y) {
@@ -48,12 +55,16 @@ class Picrypt extends JFrame
     pixel[1] = (raw >> 8) & MASK;
     pixel[2] = raw & MASK;
     
-    System.out.println("Red: " + pixel[0] + ", Green: " + pixel[1] + ", Blue: " + pixel[2]);
-    
     return pixel;
   }
   
-  public void setPixel(int x, int y, int pixel) {
+  public void setPixel(int x, int y, int[] rgb) {
+    int pixel = 0xff000000;
+    
+    pixel |= (rgb[0] << 16);
+    pixel |= (rgb[1] << 8);
+    pixel |= rgb[2];
+    
     img.setRGB(x, y, pixel);
   }
   
