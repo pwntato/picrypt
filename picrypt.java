ddaacf17790e7f57716de978728aed44e018bfcc
jimmy hendrix <jimmythedestroyer@gmail.com>
2011-08-26 22:01:43 -0700

    input and output files md5 the same

--- picrypt.java
+++ picrypt.java
@@ -7,6 +7,8 @@ import java.awt.image.BufferedImage;
 class Picrypt extends JFrame 
 {	
   private BufferedImage img;
+  private int position = 0;
+  private int color_band = 0;
 
   public Picrypt() {
     try {
@@ -15,26 +17,17 @@ class Picrypt extends JFrame
     
     FileInputStream in = null;
     byte[] input = new byte[1048576];
+    int length = 0;
     try {
-      in = new FileInputStream("resume.doc");
-      in.read(input, 0, 1048576);
+      in = new FileInputStream("sample.doc");
+      length = in.read(input, 0, 1048576);
+      
+      embedBytes(input, 0, length);
     }
     catch (Exception e) { System.out.println(e); }
     finally {
       if (in != null) {
           try { in.close(); } catch (Exception e) {}
-      }
-    }
-    
-    for (int x=0; x < img.getWidth(); x++) {
-      for (int y=0; y < img.getHeight(); y++) {
-        int[] pixel = getPixel(x, y);
-        
-        pixel[0] |= 0x00000003;
-        pixel[1] |= 0x00000003;
-        pixel[2] |= 0x00000003;
-        
-        setPixel(x, y, pixel);
       }
     }
     
@@ -47,17 +40,91 @@ class Picrypt extends JFrame
         img = ImageIO.read(new File("output.png"));
     } catch (IOException e) { System.out.println(e); }
     
-    for (int x=0; x < img.getWidth(); x++) {
-      for (int y=0; y < img.getHeight(); y++) {
-        int[] pixel = getPixel(x, y);
+    try {
+      color_band = 0;
+      byte[] data = extractBytes(0, length);
+      FileOutputStream outputfile = new FileOutputStream("output.doc");
+      outputfile.write(data, 0, length);
+    } catch (IOException e) { System.out.println(e); }
+  }
+  
+  public byte[] extractBytes(int offset, int length) {
+    int position = offset;
+    int x = position % img.getWidth();
+    int y = position / img.getHeight();
+    int[] pixel = getPixel(x, y);
+    int bit_pair = 0;
+    int working_byte = 0;
+    byte[] data = new byte[length];
+    
+    for(int cur_byte=0; cur_byte<length; cur_byte++) {
+      working_byte = 0; 
+      for (int cur_bits=3; cur_bits>=0; cur_bits--) {
+        if (color_band > 2) {           
+          color_band = 0;
+          position++;
         
-        pixel[0] &= 0x00000003;
-        pixel[1] &= 0x00000003;
-        pixel[2] &= 0x00000003;
+          x = position % img.getWidth();
+          y = position / img.getHeight();
+          
+          pixel = getPixel(x, y);
+        }
         
-        System.out.println("Red: " + pixel[0] + ", Green: " + pixel[1] + ", Blue: " + pixel[2]);
+        bit_pair = pixel[color_band];
+        bit_pair &= 0x00000003;
+        bit_pair <<= 2 * cur_bits;
+        working_byte += bit_pair;
+        
+        color_band++;
+      }
+      data[cur_byte] = (byte)working_byte;
+    }  
+    
+    return data;
+  }
+  
+  public void printPixel(int x, int y, int[] pixel) {
+    System.out.println("Pixel (" + x + ", " + y + "): " + pixel[0] + ", " + pixel[1] + ", " + pixel[2]);
+  }
+  
+  public void embedBytes(byte[] data, int offset, int length) {
+    int position = offset;
+    int x = position % img.getWidth();
+    int y = position / img.getHeight();
+    int[] pixel = getPixel(x, y);
+    int bit_pair;
+    color_band = 0;
+    
+    for(int cur_byte=0; cur_byte<length; cur_byte++) {
+      for (int cur_bits=3; cur_bits>=0; cur_bits--) {
+        bit_pair = data[cur_byte] >> (2 * cur_bits);
+        bit_pair &= 0x00000003;
+           
+        if (color_band > 2) {        
+          color_band = 0;
+          position++;
+        
+          x = position % img.getWidth();
+          y = position / img.getHeight();
+          
+          pixel = getPixel(x, y);
+        }
+        
+        pixel[color_band] &= 0xfffffffc;
+        pixel[color_band] += bit_pair;
+        setPixel(x, y, pixel);
+        
+        color_band++;
       }
     }
+  }
+  
+  public void printBinary(int data) {
+    String inBinary = "";
+    for (int i=31; i>=0; i--) {
+      inBinary += (data >> i) & 0x00000001;
+    }
+    System.out.println(inBinary);
   }
   
   public int[] getPixel(int x, int y) {
